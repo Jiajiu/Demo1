f30235c74e0122c8b55fdb4adff5c438ade5dc41
jiu <1358329310@qq.com>
2020-12-20 20:32:12 +0800

    第一次提交

---
+++ main/java/test.java
@@ -0,0 +1,10 @@
+/**
+ * @ClassName test
+ * @Author Jiu
+ * @Create 2020/12/20 20:27
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
